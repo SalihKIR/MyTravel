a674c921f64257b9cd52a3412986f845356f23fb
Salih KIR <salihkir2020@gmail.com>
2022-04-15 00:29:27 +0300

    New Update

--- mYTRAVEL/ViewController.swift
+++ mYTRAVEL/ViewController.swift
@@ -1,20 +1,48 @@
-//
-//  ViewController.swift
-//  mYTRAVEL
-//
-//  Created by Salih KIR on 14.04.2022.
-//
-
 import UIKit
 import MapKit
-class ViewController: UIViewController , MKMapViewDelegate{
+import CoreLocation
+class ViewController: UIViewController , MKMapViewDelegate , CLLocationManagerDelegate{
+    @IBOutlet weak var nameTextfield: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
     @IBOutlet weak var mapVC: MKMapView!
+    var locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         mapVC.delegate = self
-        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // Location için en iyi tahmin  parametresi.
+        //locationManager.requestWhenInUseAuthorization()// Kullanıcıdan kullanım izni iistemek için kullanılmaktadır.
+        locationManager.startUpdatingLocation()//Kullanıcının yerini belirlemeye başladık.
+        // Dokunma işlemleri
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action:#selector(choselotation(gestureRecognizer:)))
+            gestureRecognizer.minimumPressDuration = 3
+        mapVC.addGestureRecognizer(gestureRecognizer)
+            }
+    
+    // Dokunma Func
+    @objc func choselotation(gestureRecognizer:UILongPressGestureRecognizer){
+        if gestureRecognizer.state == .began{
+            let  touchpoint = gestureRecognizer.location(in: self.mapVC)
+            let  touchCordination = mapVC.convert(touchpoint, toCoordinateFrom: self.mapVC)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchCordination
+            annotation.title = "Sucess..."
+            annotation.subtitle = "True"
+            self.mapVC.addAnnotation(annotation)
+        }
     }
-
-
+    
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)//Enlem,Boylam istenilen kordinat enlem boylam için ve bulunduğun konum için.
+        let span = MKCoordinateSpan(latitudeDelta: 0.001 , longitudeDelta: 0.001)//Ekranda görünen zoom seviyeisi için.
+        //BU kod bloğu ise belirlenen zoom seviyesi gerçekleşmesini sağlayan bloktur.
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        mapVC.setRegion(region, animated: true) // haritanın set edilme işlemi gerçekleştirildi.
+    }
 }
 
